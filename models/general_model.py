ae9c6d01d6f111f50ddfeed7e546c59ead11dd1f
danibacaicoa <dbacaico@ing.uc3m.es>
2023-03-29 09:27:47 +0200

    Some changes

--- models/general_model.py
+++ models/general_model.py
@@ -1,13 +1,11 @@
 import torch.nn as nn
-#import torch.nn.init as init
-import torch.nn.functional as F
-
 
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_sizes, output_size, dropout_p=0.0,
-                 activation = 'relu', layer_init=nn.init.xavier_uniform_, bn_init=init.ones_,
+    def __init__(self, input_size, hidden_sizes, output_size, dropout_p=0.0, bn = True,
+                 activation = 'relu', layer_init=nn.init.xavier_uniform_, bn_init=nn.init.ones_,
                  bn_momentum = 0.1, seed = None):
         super().__init__()
+        self.bn = bn
         if seed is not None:
             torch.manual_seed(seed)
 
@@ -17,34 +15,30 @@ class MLP(nn.Module):
         else:
             layer_sizes = [input_size] + [hidden_sizes] + [output_size]
 
-        # Create a list of linear layers using ModuleList
+        # We create a list of linear layers and initialize their weights
         self.layers = nn.ModuleList([
             nn.Linear(layer_sizes[i], layer_sizes[i + 1])
             for i in range(len(layer_sizes) - 1)
         ])
-        # Initialize layers using given initialization function
         for layer in self.layers:
             layer_init(layer.weight)
 
-        # Create a list of batch normalization layers using ModuleList
+        # We create a list of batch normalization layers and initialize their weights
+        if self.bn:
         self.batch_norms = nn.ModuleList([
-            nn.BatchNorm1d(layer_sizes[i + 1], momentum=bn_momentum)
+            nn.BatchNorm1d(layer_sizes[i + 1], momentum = bn_momentum)
             for i in range(len(hidden_sizes))
         ])
-
-        # Initialize batch normalization layers using given initialization function
         for bn in self.batch_norms:
             bn_init(bn.weight)
 
         # Create a dropout layer
         self.dropout = nn.Dropout(dropout_p)
 
-
         self.activation = activation
         self.bn = bn
 
     def forward(self, x):
-        # Iterate over the linear layers and apply them sequentially to the input
         for i in range(len(self.layers) - 1):
             x = self.layers[i](x)
             if self.bn:
@@ -52,13 +46,16 @@ class MLP(nn.Module):
             activation_fn = getattr(nn.functional, self.activation)
             x = activation_fn(x)
             x = self.dropout(x)
-        # Apply the final linear layer to get the output
         x = self.layers[-1](x)
         return x
 
-
-# We can instantiate to have mlp_phi
-# mlp_phi = MLP(n_inputs=4, n_outputs=2, layer_sizes=[300, 301, 302, 303], dropout_p=0.0, activation_fn=F.relu,
-#              layer_init=init.xavier_uniform_, bn_init=init.ones_, bn_momentum=0.1)
+## For using Valen's mlp: (the seed is set for reproducibility reasons even if the authors didn't use it)
+## Benchmark models [nmist,knmist,fnmist]
+##    Partialization =  rand (instance independent), they use mlp_feature that can be generated instantiating MLP as:
+##      mlp_feature = MLP(input_dim, [hidden_dim, hidden_dim, hidden_dim], output_dim, dropout_p = 0.0, bn = False, seed = 1,
+##             layer_init = lambda x: nn.init.kaiming_uniform_(x, a=math.sqrt(5))
+## Benchmark models [nmist,knmist,fnmist]
+##    Partialization =  feature (instance dependent), they use mlp_phi that can be generated instantiating MLP as:
+##       mlp_phi = MLP(input_dim, hidden_sizes=[300, 301, 302, 303],  output_dim, dropout_p = 0.0, seed = 1)
 #
-#mlp_feature = MLP(input_dim, [hidden_dim, hidden_dim, hidden_dim], output_dim, activations=[nn.ReLU(), nn.ReLU(), nn.ReLU()])
+#
