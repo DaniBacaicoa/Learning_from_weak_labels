943ff9131cb324e89773e43981ca2ea743a01877
danibacaicoa <58915265+DaniBacaicoa@users.noreply.github.com>
2024-04-10 13:14:15 +0200

    dataset.py

---
+++ Dataset_generation.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+import datasets.datasets as dtset
+import utils.losses as losses
+
+from utils.weakener import Weakener
+from models.model import MLP
+
+Data = dtset.Torch_Dataset('mnist', batch_size = 16)
+Weak = Weakener(Data.num_classes)
+Weak.generate_M(model_class='pll',pll_p=0.2)
+train_X,train_y,test_X,test_y =  Data.get_data()
+Weak.generate_weak(train_y) #z and w
+
+
+import pickle
+Dataset = [Data,Weak]
+f = open("Experimental_results/Datasets.pkl","wb")
+pickle.dump(Dataset,f)
+f.close()
